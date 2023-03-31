b62d504229817000c7c785b83af25e7be815182d
Rodrigo Silveira <rodrigo.garra77@hotmail.com>
2023-03-31 11:50:34 -0300

    complete but error new image

--- script.js
+++ script.js
@@ -8,8 +8,20 @@ $(document).ready(function(){
     })
 
     $('form').on('submit', function(e){
-        console.log('submit')
         e.preventDefault()
+        const inputUrlImg = $('urlImage').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src='${inputUrlImg}'/>`).appendTo(novoItem);
+        $(
+            `<div class='imgLink'
+                <a href="${inputUrlImg}" target="_blank title="Ver imagem em tamanho real"
+                Ver imagem em tamanho real
+                </a>
+            </div>`
+        ).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#urlImage').val('');
     })
 
 })
